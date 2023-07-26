f8d5a8ec7fae3e7fc1a91c096da4e14f9d827cd6
Hachem <hachemdz25@gmail.com>
2023-07-26 16:24:44 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 6.2;
+let num2 = 10; 
+let num3 = 16;
+let num4 = 6;
 
-// Write your code here
+
+let multiply = num1 * num2
+let random = Math.ceil(Math.random() * 10)
+let mod = num3 % num4;
+let max = Math.max (2, 5, 7, 10, 14, 18, 20)
+
+console.log(multiply);
+console.log(random)
+console.log(mod);
+console.log(max);
